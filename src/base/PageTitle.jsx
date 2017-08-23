afa63b4d267f7defeac36bc6f0a5f307ec7884a3
dark-slayers <23534878@qq.com>
2017-08-23 11:36:00 +0800

    修正了一个TitlePage组件中的CSS错误

--- src/base/PageTitle.jsx
+++ src/base/PageTitle.jsx
@@ -6,8 +6,8 @@ import React from 'react'
 class PageTitle extends React.Component {
   render() {
     let divStyle = {
-      display: 'flex';
-      justifyContent: 'center';
+      display: 'flex',
+      justifyContent: 'center',
       fontSize: 20,
       color: "#00F",
       marginTop: 20,
