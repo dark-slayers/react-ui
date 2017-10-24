a9e4f84f3cb59ec4f3461d23709e01f3f269269f
dark-slayers <23534878@qq.com>
2017-10-24 11:50:28 +0800

    修改文本输入框，增加样式调节功能

--- src/form/TextInput.jsx
+++ src/form/TextInput.jsx
@@ -8,10 +8,26 @@ class TextInput extends React.Component {
       let obj = this.props.obj;
       const name = this.props.name;
       const label = this.props.label;
+      const labelWidth=this.props.labelWidth?this.props.labelWidth:100;
+      const inputWidth=this.props.inputWidth?this.props.inputWidth:100;
+      const labelStyle = {
+        display: 'inline-flex',
+        justifyContent: 'flex-end',
+        width: labelWidth
+      };
+      const inputStyle = {
+        display: 'inline-flex',
+        width: inputWidth,
+        borderColor: '#000',
+        borderWidth: 1,
+        borderStyle: 'solid',
+        padding: 1
+      };
       return (
-          <label>{label}
-            <input type="text" name={name} value={obj.state[name]} onChange={obj.handleTextChange}/>
-          </label>
+        <div>
+          <span style={labelStyle}>{label}</span>
+          <input style={inputStyle} type="text" name={name} value={obj.state[name]} onChange={obj.handleTextChange}/>
+        </div>
       );
   }
 }
