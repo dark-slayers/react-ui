b1c646743edb3de04376a6d8931eccab5cd25eaf
dark-slayers <23534878@qq.com>
2017-10-24 15:03:05 +0800

    修复IdTextInput组件中缺少导入模块的问题

--- src/form/IdTextInput.jsx
+++ src/form/IdTextInput.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
-/**基本的文本输入框，输入框需要提供：label、name、obj三个属性
+import TextInput from './TextInput.jsx';
+/**标识符文本输入框，输入框需要提供：label、name、obj、addModel四个属性
 *label为输入框标签，name对应obj对象state参数相应KEY,
-*obj为调用此输入框的组件，obj需要有handleChange方法或者继承handleChange方法
+*obj为调用此输入框的组件，obj需要有handleChange方法或者继承handleChange方法，
+* addModel表示组件是否处于添加模式，处于添加模式下的组件可以输入，非添加模式不可编辑
 */
 class IdTextInput extends TextInput {
   isAddModel=()=>{
